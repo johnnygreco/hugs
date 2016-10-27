a3719756684d6920df6bc790239d5bd7e6f39e6e
Johnny Greco <jgreco@astro.princeton.edu>
2016-10-26 23:58:50 -0400

    test gauss smooth

--- hugs_pipe/imtools/tests/test_core.py
+++ hugs_pipe/imtools/tests/test_core.py
@@ -3,8 +3,9 @@ Unit tests for hugs.imtools
 """
 from __future__ import division, print_function
 
+import numpy as np
 from ...utils import get_test_exp
-from ..core import get_cutout
+from ..core import get_cutout, smooth_gauss
 
 exposure = get_test_exp()
 
@@ -14,3 +15,11 @@ def test_get_cutout():
     cutout = get_cutout(center, size, exp=exposure)
     img = cutout.getMaskedImage().getImage().getArray()
     assert img.shape == (435, 419)
+
+
+def test_smooth_gauss():
+    mi = exposure.getMaskedImage()
+    mi_smooth = smooth_gauss(mi, 10.0)
+    arr = mi.getImage().getArray()
+    arr_smooth = mi_smooth.getImage().getArray()
+    assert np.nanstd(arr) > np.nanstd(arr_smooth)
