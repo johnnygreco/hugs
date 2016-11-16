68f122b052b2c8d381a870a83712ebddb8d27f48
Johnny Greco <jgreco@astro.princeton.edu>
2016-11-16 15:44:31 -0500

    parser added

--- hugs_pipe/__init__.py
+++ hugs_pipe/__init__.py
@@ -2,6 +2,7 @@ from . import viz
 from . import imtools
 from . import stats
 from . import synths
+from .parser import parse_args
 from .synths import SynthFactory
 from .stats import get_clipped_sig_task
 from .run import run
---
+++ hugs_pipe/parser.py
@@ -0,0 +1,49 @@
+from __future__ import division, print_function
+
+import os, sys
+from argparse import ArgumentParser
+from .utils import io
+
+def parse_args(default_outdir=io):
+    parser = ArgumentParser('python '+sys.argv[0]) 
+    parser.add_argument('-t', 
+                        '--tract', 
+                        type=int, 
+                        help='HSC tract')
+    parser.add_argument('-p', 
+                        '--patch', 
+                        type=str, 
+                        help='HSC patch')
+    parser.add_argument('-g', 
+                        '--group_id', 
+                        type=int, 
+                        help='group id', 
+                        default=None)
+    parser.add_argument('-c', 
+                        '--config_fn', 
+                        type=str, 
+                        help='config file name', 
+                        default=None)
+    parser.add_argument('-o', 
+                        '--outdir', 
+                        type=str, 
+                        help='output directory', 
+                        default=default_outdir)
+    parser.add_argument('-s', 
+                        '--seed', 
+                        type=int, 
+                        help='rng seed', 
+                        default= None)
+    parser.add_argument('--num_synths', 
+                        type=int, 
+                        help='number of synths to inject', 
+                        default=10)
+    args = parser.parse_args()
+
+    if args.group_id:
+        args.group_dir = os.path.join(args.outdir, 'group_'+args.group_id)
+    elif (args.tract is None) or (args.patch is None):
+        parser.print_help()
+        exit(0)
+
+    return args
