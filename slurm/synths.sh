c40cb85f44503b350fcd8f7b1c70f1acd26a4b04
Johnny Greco <jgreco@astro.princeton.edu>
2016-11-21 18:17:54 -0500

    loop over labels

--- slurm/synths.sh
+++ slurm/synths.sh
@@ -2,8 +2,8 @@
 # 
 #SBATCH -J synths      # job name
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=10
-#SBATCH -t 1:00:00 
+#SBATCH --ntasks-per-node=16
+#SBATCH -t 3:00:00 
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=jgreco@princeton.edu 
@@ -12,6 +12,11 @@ cd /home/jgreco/projects/hugs-pipe/scripts
 
 group_id=4736
 num_synths=15
+nrepeat=10
 
-python synths.py --ncores 10 -g $group_id --num_synths $num_synths\
-    -c $HUGS_PIPE_IO/synth-results/config-11-17-2016.yml
+for label in $(seq 1 $nrepeat); do
+    python synths.py --ncores 16 -g $group_id --num_synths $num_synths --label $label \
+        -c $HUGS_PIPE_IO/synth-results/config-11-17-2016.yml \
+        -o /scratch/network/jgreco/hugs-pipe-output/synth-results
+done
+wait
