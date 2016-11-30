492286eb68cdb9ef063cf170222edf01000031c8
Johnny Greco <jgreco@astro.princeton.edu>
2016-11-30 13:02:16 -0500

    go to 50

--- slurm/synths.sh
+++ slurm/synths.sh
@@ -1,9 +1,9 @@
 #! /bin/bash 
 # 
 #SBATCH -J synths      # job name
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH --ntasks-per-node=16
-#SBATCH -t 2:00:00 
+#SBATCH -t 5:00:00 
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=jgreco@princeton.edu 
@@ -17,10 +17,10 @@ fi
 
 group_id=$1
 num_synths=15
-nrepeat=10
+nrepeat=50
 
-for label in $(seq 1 $nrepeat); do
-    python synths.py --ncores 16 -g $group_id --num_synths $num_synths --label $label \
+for label in $(seq 11 $nrepeat); do
+    python synths.py --ncores 32 -g $group_id --num_synths $num_synths --label $label \
         -c $HUGS_PIPE_IO/synth-results/config-11-17-2016.yml \
         -o /scratch/network/jgreco/hugs-pipe-output/synth-results
 done
