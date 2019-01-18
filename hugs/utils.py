d339b243a6dbfbc9162350e6463bc23a306f33fa
Johnny Greco <jgreco.astro@gmail.com>
2019-01-18 15:45:19 -0500

    added angsep function

--- hugs/utils.py
+++ hugs/utils.py
@@ -22,6 +22,7 @@ zpt = 27.0
 ExtCoeff = namedtuple('ExtCoeff', 'g r i z y')
 ext_coeff = ExtCoeff(g=3.233, r=2.291, i=1.635, z=1.261, y=1.076)
 
+
 # Patch metadata
 PatchMeta = namedtuple(
     'PatchMeta', 'x0 y0 cleaned_frac bright_obj_frac good_data_frac')
@@ -347,3 +348,87 @@ def make_noise_image(masked_image, random_state=None):
     dims = masked_image.getDimensions()
     noise_array = back_rms*rng.randn(dims[1], dims[0])
     return noise_array
+
+
+def solid_angle(ra_lim, dec_lim):
+    """
+    Calculate solid angle with given ra & dec range.
+    All angles in degrees.
+
+    Parameters
+    ----------
+    ra_lim : list-like, optional
+        ra limits.
+    dec_lim : list-like, optional
+        dec limits.
+    Returns
+    -------
+    area : float
+        Solid angle in square degrees.
+    """
+    ra_lim = np.deg2rad(np.asarray(ra_lim))
+    dec_lim = np.deg2rad(np.asarray(dec_lim))
+    dsin_dec = np.sin(dec_lim[1]) - np.sin(dec_lim[0])
+    area = ra_lim.ptp() * dsin_dec * (180.0/np.pi)**2
+    return area
+
+
+def angsep(ra1, dec1, ra2, dec2, sepunits='arcsec'):
+    """
+    Return angular separation btwn points
+
+    Inputs
+    ------
+    ra1 :  float or ndarray
+        Right ascension(s) of first point(s) (in degrees). The shape
+        must be the same as dec1.
+    dec1 : float or ndarray
+        Declination(s) of first point(s) (in degrees). The shape must
+        be the same as ra1.
+    ra2 :  float or ndarray
+        Right ascension(s) of second(s) point (in degrees). The shape
+        must be the same as dec2.
+    dec2 : float or ndarray
+        Declination(s) of second point(s) (in degrees). The shape must
+        be the same as ra2.
+    sepunits : string, optional, default = 'arcsec'
+        Angular unit of the returned separation
+        (radian, degree, arcsec, or arcmin)
+
+    Returns
+    -------
+    sep : same type as input angles, angular separation between points
+
+    Note
+    ----
+    This is function uses the Vincenty formula:
+    https://en.wikipedia.org/wiki/Great-circle_distance.
+    """
+
+    deg2rad = np.pi/180.0
+
+    ra1 = ra1*deg2rad
+    dec1 = dec1*deg2rad
+    ra2 = ra2*deg2rad
+    dec2 = dec2*deg2rad
+
+    sin_dRA = np.sin(ra2 - ra1)
+    cos_dRA = np.cos(ra2 - ra1)
+    sin_dec1 = np.sin(dec1)
+    sin_dec2 = np.sin(dec2)
+    cos_dec1 = np.cos(dec1)
+    cos_dec2 = np.cos(dec2)
+
+    num1 = cos_dec2*sin_dRA
+    num2 = cos_dec1*sin_dec2 - sin_dec1*cos_dec2*cos_dRA
+    denom = sin_dec1*sin_dec2 + cos_dec1*cos_dec2*cos_dRA
+    sep = np.arctan2(np.sqrt(num1*num1 + num2*num2), denom)
+
+    conversion = {'radian':1.0, 
+                  'arcsec':206264.806, 
+                  'arcmin':206264.806/60.0, 
+                  'degree':180./np.pi}
+
+    sep *= conversion[sepunits]
+
+    return sep
