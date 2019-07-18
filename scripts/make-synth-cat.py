ccffe346804f8290ab8f7addc64c249d7fac984c
Johnny Greco <greco.40@osu.edu>
2019-07-18 10:23:14 -0400

    pass color type synth cat maker

--- scripts/make-synth-cat.py
+++ scripts/make-synth-cat.py
@@ -1,8 +1,15 @@
 import hugs
 from argparse import ArgumentParser
 
+color_dict = dict(
+    blues={'g_i': 0.47, 'g_r': 0.32},
+    reds={'g_i': 0.82, 'g_r': 0.56},
+    med={'g_i': 0.64, 'g_r': 0.43},
+)
+
 parser = ArgumentParser()
 parser.add_argument('-o', '--out-fn', dest='out_fn', required=True)
+parser.add_argument('--synth-color', required=True)
 parser.add_argument('-c', '--config-fn', dest='config_fn', default=None, 
                     help='non-defulat synth parameters')
 args = parser.parse_args()
@@ -20,6 +27,8 @@ else:
                   theta_range=[0, 180], 
                   min_sep=15, 
                   mu_type='average')
+
+kwargs.update(color_dict[args.synth_color])
 
 cat = hugs.synths.catalog.build_catalog_survey(**kwargs)
 syncat = hugs.synths.catalog.GlobalSynthCat(catalog=cat)
